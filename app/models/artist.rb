01c93bb13c1f8202634d23f971914d6c037bab79
bcharlotin1 <biancacharlotin1@gmail.com>
2021-02-26 15:41:10 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
+# Artists have many songs and a song belongs to an artist.
+# Artists have many genres through songs.
 class Artist < ActiveRecord::Base
+    has_many :songs
+  has_many :genres, through: :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
+# Songs belong to a genre.
+#Songs belong to artist
 class Song < ActiveRecord::Base
+    belongs_to :artist
+    belongs_to :genre
+
 
 end
